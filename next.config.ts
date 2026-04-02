cfdc475bd8ee84ebdf91d7d1830c3fb91b24a78b
Sudeep T Pillai <sudeep266133@gmail.com>
2026-04-02 09:55:38 +0530

    trying with vercel

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,5 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  basePath: "/Fallback-Portfolio" ,
-  output: "export",
 }
 export default nextConfig;
